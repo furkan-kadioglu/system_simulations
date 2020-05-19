cd0f41ac6b607d25bd4a1c495d973e78091e1356
melihaydogd <ahaydogdu@hotmail.com>
2020-05-20 00:27:56 +0300

    ks test fixed

--- Aydoğdu_Bozdoğan_Kadıoğlu.R
+++ Aydoğdu_Bozdoğan_Kadıoğlu.R
@@ -2,7 +2,7 @@
 #install.packages("xlsx")
 
 # Load Data
-library("xlsx")
+library("xlsx") # This package is used to read data from .xls file.
 data <- read.xlsx("Assignment2-Interarrival_Data-S2020.xls",
                  sheetIndex = 1)
 day1=data[,1]
@@ -11,15 +11,20 @@ day2=data[,2]
     
 #------------------------1 Kolmogorov-Smirnovtest-------------------#
 uniformity_test = function(x){
-    x.sorted = sort(x)
+    x = sort(x)
+    x = punif(x, min = 0, max = 400)
     x.length = length(x)
 
     D.plus = max(((1:x.length) / x.length) - x)
     D.minus = max(x - ((1:x.length - 1) / x.length))
     D = max(D.minus, D.plus)
 
+    print(D)
+    print(1.36 / sqrt(x.length))
     print(D < (1.36 / sqrt(x.length)))
 }
+#uniformity_test(day1)
+#uniformity_test(day2)
 
 #-------------------------2 Sample Statistics-----------------------#
 append = function(x,y) {
@@ -30,7 +35,7 @@ statistics = summary(data)
 statistics = rbind(statistics, 
                     c(append("Std.   : ", round(sd(data[,1]),3)), 
                       append("Std.   : ", round(sd(data[,2]),3))))
-
+                      
 #-------------------------3 Histogram------------------------------#
 
 ## DAY 1 Histograms
@@ -45,7 +50,8 @@ hist(data[,2], xlab='Day 2 for 20 secs', xlim=c(0,400), breaks=400/20)
 
 #----------------------4 Chi-Square Test---------------------------#
 frequency = function (x) {
-    x.mean = mean(data[,2])
+    #x.mean = mean(data[,1]) # day 1
+    x.mean = mean(data[,2]) # day 2
     pexp(x+10, rate=1/x.mean) - pexp(x, rate=1/x.mean)
 }
 
@@ -57,13 +63,17 @@ chi_square_test = function(O, interval = 10){
 
     E.frequency = unlist(lapply(0:end * interval, frequency)) * N
 
+
     diff = O.frequency - E.frequency
     X = sum(((diff)**2) / E.frequency)
 
     print(X)
-    print(X < 48.602)  # day 2
-    #print(X < 60.481)  # day 1
+    #print(X < 58.142)  # day 1
+    print(X < 46.194)  # day 2
 }
+
+#chi_square_test(day1)
+#chi_square_test(day2)
 
 #----------------------5 QQ Plot----------------------------------#
 
@@ -71,14 +81,22 @@ qq_plot = function(x){
     x.length = length(x)
     x.mean = mean(x)
     theoretical = qexp(ppoints(x.length),rate=1/x.mean)
-    qqplot(x, theoretical)
+    qqplot(x, theoretical, xlab="Interarrival Times", ylab="Exponential Data Quantiles")
+    a = x
+    lines(x,a)
 }
+
+#qq_plot(day1)
+#qq_plot(day2)
+
 
 #--------------6 inter-arrival vs observation times---------------#
 plot_inter = function(x){
-    plot(cumsum(x), x, type="h")
+    plot(cumsum(x), x, type="h", xlab="Observation Times", ylab="Interarrival Times")
 }
 
+#plot_inter(day1)
+#plot_inter(day2)
 
 #------------------------7 Auto correlation-----------------------#
 
@@ -89,8 +107,12 @@ autocorrelation = function(arr, lag){
     ro = sum(unlist(lapply(0:M, function(k){arr[1+k*lag]*arr[1+(k+1)*lag]})))/(M+1)-0.25
     sigma = sqrt(13*M+7)/(12*(M+1))
     z0 = ro/sigma
-    if (z0 >= -1.96 && z0 <= 1.96)
+    if (z0 >= -1.96 && z0 <= 1.96) 
         sprintf("Data seems independent at alpha=0.05. |%f|<1.96", z0)
     else
         sprintf("Data seems not independent at alpha=0.05. |%f|>1.96", z0)
 }
+
+#autocorrelation(day1)
+#autocorrelation(day2)
+
